4c2aed7be5fffc4b9e1f1ed243c0263398e67342
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2024-10-23 11:45:47 +0700

    git init project

---
+++ src/main/java/id/anggra/belajarspringrestapi/BelajarSpringRestApiApplication.java
@@ -0,0 +1,15 @@
+package id.anggra.belajarspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringRestApiApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BelajarSpringRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/anggra/belajarspringrestapi/BelajarSpringRestApiApplicationTests.java
@@ -0,0 +1,15 @@
+package id.anggra.belajarspringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRestApiApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
